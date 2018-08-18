2c6920c2dadb9ae532ba52e6d62a81f45777c3ac
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-18 05:19:22 +0200

    Update lib/butter_cms.rb file and load ButterCMS::Parsers::TagsObjects

--- lib/butter_cms.rb
+++ lib/butter_cms.rb
@@ -8,6 +8,8 @@ require 'butter_cms/author'
 require 'butter_cms/parsers/author_object'
 require 'butter_cms/category'
 require 'butter_cms/parsers/categories_objects'
+require 'butter_cms/tag'
+require 'butter_cms/parsers/tags_objects'
 
 module ButterCMS
   class << self
