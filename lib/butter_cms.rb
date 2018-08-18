545fa811b4fccbaf9169411479982ac1b979b92a
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-18 04:46:52 +0200

    Update lib/butter_cms.rb and load resource class

--- lib/butter_cms.rb
+++ lib/butter_cms.rb
@@ -3,6 +3,7 @@ require 'butter_cms/url_params_service'
 require 'butter_cms/requests/api'
 require 'butter_cms/requests/get'
 require 'butter_cms/parsers/posts'
+require 'butter_cms/resource'
 
 module ButterCMS
   class << self
