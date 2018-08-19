9bd406f85df6bb72820109d7b401a80286457ebd
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-19 14:46:35 +0200

    Update ButterCMS::Parsers::Posts and add documentation comments

--- lib/butter_cms/parsers/posts.rb
+++ lib/butter_cms/parsers/posts.rb
@@ -8,18 +8,30 @@ module ButterCMS
         @response = response
       end
 
+      # Returns the number of the next page or nil if not available
+      #
+      # @return [String]
       def next_page
         parsed_json['meta']['next_page']
       end
 
+      # Returns the number of the previous page or nil if not available
+      #
+      # @return [String]
       def previous_page
         parsed_json['meta']['previous_page']
       end
 
+      # Returns the count of existing posts
+      #
+      # @return [String]
       def count
         parsed_json['meta']['count']
       end
 
+      # Returns array of posts attributes available in the response
+      #
+      # @return [Array]
       def posts
         parsed_json['data']
       end
