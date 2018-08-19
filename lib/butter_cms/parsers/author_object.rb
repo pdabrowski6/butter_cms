a7bc425aee71d18c6e904f3997768d85508243c2
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-19 14:46:11 +0200

    Update ButterCMS::Parsers::AuthorObject and add documentation comments

--- lib/butter_cms/parsers/author_object.rb
+++ lib/butter_cms/parsers/author_object.rb
@@ -1,7 +1,9 @@
 module ButterCMS
   module Parsers
     class AuthorObject
-
+      # Returns the new instance of author object from the given attributes
+      #
+      # @return [ButterCMS::Author]
       def self.call(author)
         ::ButterCMS::Author.new(author)
       end
