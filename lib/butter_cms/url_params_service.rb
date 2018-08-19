586cb4ab5178a3d3d31a8c8a80901549c5a5cc5e
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-19 14:47:20 +0200

    Update ButterCMS::UrlParamsService and add documentation comments

--- lib/butter_cms/url_params_service.rb
+++ lib/butter_cms/url_params_service.rb
@@ -1,6 +1,9 @@
 module ButterCMS
   class UrlParamsService
 
+    # Returns the query part of the request to the API
+    #
+    # @return [String]
     def self.call(options = {})
       options[:auth_token] = ButterCMS.configuration.api_key
 
