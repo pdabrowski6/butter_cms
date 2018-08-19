741d88dfb3f4bfe2921877468bc8e1de624d8f10
Paweł Dąbrowski <dziamber@gmail.com>
2018-08-19 14:46:19 +0200

    Update ButterCMS::Parsers::CategoriesObject and add documentation comments

--- lib/butter_cms/parsers/categories_objects.rb
+++ lib/butter_cms/parsers/categories_objects.rb
@@ -1,7 +1,9 @@
 module ButterCMS
   module Parsers
     class CategoriesObjects
-
+      # Returns array of category objects created from given array of attributes
+      #
+      # @return [Array<ButterCMS::Category>]
       def self.call(categories)
         categories.map do |category_attributes|
           ::ButterCMS::Category.new(category_attributes)
